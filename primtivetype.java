b2ec02bc063f72d4786b81621c017f6bdfd8b0bb
chandu11215 <kalalshannu19@gmail.com>
2020-06-21 13:49:45 +0530

    pritive datatypes related

---
+++ primtivetype.java
@@ -0,0 +1,10 @@
+public class PrimitiveChallenge{
+
+   public static void main(String[] args)
+    byte byteValue = 10;
+    short shortValue = 20;
+    int intValue =50;
+
+    long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+    System.out.println(longTotal);
+
